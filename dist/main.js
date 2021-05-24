49f101fbec72097c6d0beca7dd29f690bc15cb2e
bahaabder <bahaa.bder97@gmail.com>
2021-05-24 12:46:26 +0300

    add Comment

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 const renderRecipes = new Render()
-
+    //Get Data From Server
 const search = function() {
     let input = $("#ingredientInput").val()
     $.get(`/recipes/${input}`, function(recipes) {
